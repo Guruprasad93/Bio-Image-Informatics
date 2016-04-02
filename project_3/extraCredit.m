d78d8caa73254b88b97fd91f5d40e9c513f4b7fb
priyankaraja <priyanka.8916@gmail.com>
2016-04-02 18:09:05 -0400

    Sub-pixel & Extra credit

--- project_3/extraCredit.m
+++ project_3/extraCredit.m
@@ -1,8 +1,8 @@
 %Extra credit Problem
 
-function [Algo_subPixelMaxima, Real_subPixelMaxima] = extraCredit(I_synthetic, finalMaxima)
+function [Algo_subPixelMaxima, Real_subPixelMaxima] = extraCredit(I_synthetic, I_syntheticNoNoise, finalMaxima)
 
 %Passing the (Un)convolved original image for gaussian level detection
-[Algo_subPixelMaxima, Real_subPixelMaxima] = subPixelDetection(finalMaxima, I_synthetic);
+[Algo_subPixelMaxima, Real_subPixelMaxima] = subPixelDetection(finalMaxima, I_synthetic, I_syntheticNoNoise);
 
 end
