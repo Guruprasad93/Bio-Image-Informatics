1d86ea1d67e7c5a51765c5e7b4ddc886f70846d1
priyankaraja <priyanka.8916@gmail.com>
2016-04-04 14:19:03 -0400

    I to I_detect

--- project_3/createSynthetic.m
+++ project_3/createSynthetic.m
@@ -6,16 +6,16 @@ indices = sub2ind(size(I),finalMaxima(:,1),finalMaxima(:,2));
 I_detection(indices) = finalMaxima(:,3);
 
 %gaussian conv with point-spread-function
-[I_detection] = Gaussian_filter(I,65*1e-9);
-
+[I_detection] = Gaussian_filter(I_detection,65*1e-9); 
 %White noise - mean = noise_mean; standard-dev = average of all detected localmaxima
-I_noise = randn(size(I))*noise*mean(finalMaxima(:,3)) + noise_mean;
-
+I_noise = randn(size(I))*noise*min(finalMaxima(:,3)) + noise_mean;
+% lambda = 515*1e-9; 
 %Adding noise to psf convolved detected image
 I_synthetic = I_detection + I_noise;
 
 figure,
 imshow(I_synthetic,[])
-title('Synthetic image')
+title(['Synthetic image',' : ',num2str(noise*100),' % ', 'noise']);
+
 
 end
